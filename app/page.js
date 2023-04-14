df98403069c3a5a80ff8da7400116bfd0c7ad172
WorldCreator46 <96541154+WorldCreator46@users.noreply.github.com>
2023-04-14 23:19:15 +0900

    d

--- app/page.js
+++ app/page.js
@@ -1,3 +1,8 @@
-export default function Home() {
-  return <div></div>;
+export default function Main() {
+  return (
+    <div>
+      <h1 className="title">신선물스토어 </h1>
+      <p className="title-sub">신선함이 가득한 삶을 위한 최고의 선택</p>
+    </div>
+  );
 }
